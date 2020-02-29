3a615fb731e711674a4b6dcde5b773384e8af65d
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-02-29 19:37:21 +0700

    added loops package

---
+++ loops/loops.go
@@ -0,0 +1,10 @@
+package loops
+
+import "fmt"
+
+func PrintLoops() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("i", i)
+	}
+
+}
--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import "conditional_statements_and_loops/conditional_statements"
+import (
+	"conditional_statements_and_loops/conditional_statements"
+	"conditional_statements_and_loops/loops"
+)
 
 func main() {
 	runConditionalStatements()
+	runLoops()
 }
 
 func runConditionalStatements() {
 	conditional_statements.PrintStatementIfNeeded()
+}
+
+func runLoops() {
+	loops.PrintLoops()
 }
