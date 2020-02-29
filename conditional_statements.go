38f501de3917db61620027d18a01b3777a120422
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-02-29 19:16:42 +0700

    added conditional_statements.go

---
+++ conditional_statements.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 20
+
+	if a > 0 && b > 0 {
+		fmt.Println("Values is greater than 0")
+	} else {
+		fmt.Println("Values is not greater than 0")
+	}
+}
